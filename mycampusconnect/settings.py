f4439741182925cfb079f96f867e866281f9576e
Adarsh Mishra <mishraadarsh2609@gmail.com>
2025-06-19 11:17:58 +0530

    Modified setting

--- mycampusconnect/settings.py
+++ mycampusconnect/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ← Required for collectstatic
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
